3a14cbc7eb924f100b68064d5617cad210e9d2b2
Adam Engebretson <adam.engebretson@getbridge.com>
2022-08-11 00:04:29 -0500

    Using the right bins

--- install.sh
+++ install.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-nix-channel --add https://github.com/nix-community/home-manager/archive/release-22.05.tar.gz home-manager
-nix-channel --update
+/home/gitpod/.nix-profile/bin/nix-channel --add https://github.com/nix-community/home-manager/archive/release-22.05.tar.gz home-manager
+/home/gitpod/.nix-profile/bin/nix-channel --update
 export NIX_PATH=$HOME/.nix-defexpr/channels:/nix/var/nix/profiles/per-user/root/channels${NIX_PATH:+:$NIX_PATH}
 
-nix-shell '<home-manager>' -A install
+/home/gitpod/.nix-profile/bin/nix-shell '<home-manager>' -A install
 
 home-manager switch --flake .#gitpod

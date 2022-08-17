3949fe1bc22f540bce5a4eaf4329b71bb909520d
Adam Engebretson <adam.engebretson@getbridge.com>
2022-08-16 23:31:29 -0500

    Tweaking dconf

--- home/adam/pantheon/default.nix
+++ home/adam/pantheon/default.nix
@@ -4,9 +4,10 @@ let
   mkTuple = lib.hm.gvariant.mkTuple;
 in
 {
-    dconf.enable = true;
+  dconf.enable = true;
   dconf.settings = {
     "org/gnome/settings-daemon/plugins/power" = {
+      "sleep-inactive-ac-timeout" = "0";
       "sleep-inactive-ac-type" = "never";
     };
   };

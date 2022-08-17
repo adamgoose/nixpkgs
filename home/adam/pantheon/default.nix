6b41051a6910f234af7a21bbd8147f0c6f342497
Adam Engebretson <adam@enge.me>
2022-08-17 00:40:01 -0500

    Tweaking pantheon some more

--- home/adam/pantheon/default.nix
+++ home/adam/pantheon/default.nix
@@ -6,6 +6,13 @@ in
 {
   dconf.enable = true;
   dconf.settings = {
+    "net/launchpad/plank/docks/dock1" = {
+      "icon-size" = 32;
+    };
+    "org/gnome/desktop/interface" = {
+      "clock-format" = "12h";
+      "gtk-theme" = "io.elementary.stylesheet.slate";
+    };
     "org/gnome/settings-daemon/plugins/power" = {
       "sleep-inactive-ac-timeout" = 0;
       "sleep-inactive-ac-type" = "nothing";

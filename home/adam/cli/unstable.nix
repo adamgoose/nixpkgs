effac3f7a2b3f033a6eb199311550c397f51bc0d
Adam Engebretson <adam@enge.me>
2022-08-18 00:50:49 -0500

    Conditionally installing 1Password

--- home/adam/cli/unstable.nix
+++ home/adam/cli/unstable.nix
@@ -1,5 +1,11 @@
-{ unstable, ... }: {
+{ unstable, lib, ... }: {
   home.packages = with unstable; [
-    _1password
-  ];
+    # ...
+  ]
+
+  # The 1Password CLI Must exist in /usr/local/bin on Mac for biometric unlock
+  # to work, so manual installation is required.
+  ++ lib.lists.optional (!stdenv.isDarwin) _1password
+
+  ;
 }

93cff0f83ed6a8c56770e5996378bbbf7529dba2
Adam Engebretson <adam.engebretson@getbridge.com>
2022-09-26 23:09:03 -0500

    Updating Go to unstable

--- home/adam/ide-full/default.nix
+++ home/adam/ide-full/default.nix
@@ -1,16 +1,18 @@
-{ pkgs, ... }: {
+{ pkgs, unstable, ... }: {
   imports = [
     ../ide
     ./neovim.nix
   ];
 
   home.packages = with pkgs; [
-    go_1_18
+    # go_1_18
     clang
     ctags
     cscope
     rnix-lsp
     postgresql
     kotlin-language-server
-  ];
+  ] ++ (with unstable; [
+    go_1_19
+  ]);
 }

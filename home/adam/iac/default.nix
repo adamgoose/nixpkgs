636283023fd7df14782faca1b3e09e47d070b97a
Adam Engebretson <adam.engebretson@getbridge.com>
2022-09-30 01:28:35 -0500

    Replacing tfswitch with tfenv

--- home/adam/iac/default.nix
+++ home/adam/iac/default.nix
@@ -1,21 +1,31 @@
 { pkgs, ... }: {
   home.packages = with pkgs; [
     vault
-    tfswitch
     pulumi-bin
     pulumictl
     terragrunt
     truss-cli
   ];
 
+  home.shellAliases = {
+    tf = "terraform";
+  };
+
+  programs.zsh.zplug = {
+    plugins = [{
+      name = "cda0/zsh-tfenv";
+    }];
+  };
+
   programs.neovim = {
     plugins = with pkgs.vimPlugins; [
       vim-terraform-completion
       { plugin = vim-terraform;
         config = ''
-          g:terraform_fmt_on_save = 1
+          let g:terraform_fmt_on_save = 1
         '';
       }
     ];
   };
+
 }

91cc5d4a24669c7aa5808e0c44986cff05d0f3a9
Adam Engebretson <adam.engebretson@getbridge.com>
2022-11-08 00:21:27 -0600

    Installing Trackwarrior

--- home/adam/cli/zsh.nix
+++ home/adam/cli/zsh.nix
@@ -40,4 +40,9 @@
   home.file.".oh-my-zsh/custom".source = ./files/oh-my-zsh-custom;
   home.file.".oh-my-zsh/custom".recursive = true;
 
+  home.file.".task/hooks".source = "${pkgs.trackwarrior}/taskwarrior/hooks";
+  home.file.".task/hooks".recursive = true;
+  home.file.".timewarrior/extensions".source = "${pkgs.trackwarrior}/timewarrior/extensions";
+  home.file.".timewarrior/extensions".recursive = true;
+
 }
--- pkgs/default.nix
+++ pkgs/default.nix
@@ -3,6 +3,7 @@
   kubectx = pkgs.callPackage ./kubectx {};
   oh-my-posh = pkgs.callPackage ./oh-my-posh {};
   tere = pkgs.callPackage ./tere {};
+  trackwarrior = pkgs.callPackage ./trackwarrior {};
   truss-cli = pkgs.callPackage ./truss-cli {};
   truss-local = pkgs.callPackage ./truss-local {};
   kotlin-language-server = pkgs.callPackage ./kotlin-language-server {};

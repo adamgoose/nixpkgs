de742a3f9cd134e502306facc89d2d553b604dcc
Adam Engebretson <adam.engebretson@getbridge.com>
2023-01-13 00:18:13 -0600

    Adding enc

--- pkgs/default.nix
+++ pkgs/default.nix
@@ -1,5 +1,6 @@
 # When you add custom packages, list them here
 { pkgs }: {
+  enc = pkgs.callPackage ./enc {};
   kotlin-language-server = pkgs.callPackage ./kotlin-language-server {};
   trackwarrior = pkgs.callPackage ./trackwarrior {};
   truss-cli = pkgs.callPackage ./truss-cli {};

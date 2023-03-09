2e12a9a341808c2a9a5ebfe4697e779e9ced92a3
Adam Engebretson <adam.engebretson@getbridge.com>
2023-03-08 23:32:24 -0600

    Some Bridge SSH Config

--- home/adam/adam@bridge/default.nix
+++ home/adam/adam@bridge/default.nix
@@ -3,13 +3,14 @@
 {
   programs.ssh = {
     enable = true;
+    controlPath = "~/.ssh/%C";
     matchBlocks = {
       "*" = {
         extraOptions = {
           IdentityAgent = "\"~/Library/Group Containers/2BUA8C4S2C.com.1password/t/agent.sock\"";
         };
       };
-      "*.tsh.bridgeops.sh tsh.bridgeops.sh" = lib.hm.dag.entryBefore ["*.tsh.bridgeops.sh !tsh.bridgeops.sh"] {
+      "*.tsh.bridgeops.sh tsh.bridgeops.sh" = lib.hm.dag.entryBefore [ "*.tsh.bridgeops.sh !tsh.bridgeops.sh" ] {
         identityFile = "/Users/adam/.tsh/keys/tsh.bridgeops.sh/adamgoose";
         certificateFile = "/Users/adam/.tsh/keys/tsh.bridgeops.sh/adamgoose-ssh/tsh.bridgeops.sh-cert.pub";
         extraOptions = {

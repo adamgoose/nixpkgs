303ab413d9bdd1fe72dc4d98c7813a35b65a8541
Adam Engebretson <adam@enge.me>
2023-07-28 23:12:04 -0500

    Adding some Darwin settings

--- darwin/adam/default.nix
+++ darwin/adam/default.nix
@@ -36,6 +36,12 @@ in
 
   system.defaults = {
     dock.autohide = true;
+    dock.mru-spaces = false;
+    dock.show-recents = false;
+    dock.wvous-bl-corner = 4; # Desktop
+    dock.wvous-br-corner = 2; # Mission Control
+    dock.wvous-tr-corner = 5; # Start Screen Saver
+    dock.wvous-tl-corner = 6; # Disable Screen Saver
     finder.ShowPathbar = true;
     finder.ShowStatusBar = true;
     finder._FXShowPosixPathInTitle = true;

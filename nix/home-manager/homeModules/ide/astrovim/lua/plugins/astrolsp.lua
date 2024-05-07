7ef5c7b2fea5d167aa25d5e127510236e8c03a1c
Adam Engebretson <adam@enge.me>
2024-05-07 14:17:06 -0500

    Some IDE stuff

--- nix/home-manager/homeModules/ide/astrovim/lua/plugins/astrolsp.lua
+++ nix/home-manager/homeModules/ide/astrovim/lua/plugins/astrolsp.lua
@@ -1,5 +1,3 @@
-if true then return {} end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
-
 -- AstroLSP allows you to customize the features in AstroNvim's LSP configuration engine
 -- Configuration documentation can be found with `:h astrolsp`
 -- NOTE: We highly recommend setting up the Lua Language Server (`:LspInstall lua_ls`)
@@ -46,6 +44,22 @@ return {
     ---@diagnostic disable: missing-fields
     config = {
       -- clangd = { capabilities = { offsetEncoding = "utf-8" } },
+      tsserver = {
+        init_options = {
+          plugins = {
+            {
+              name = "@vue/typescript-plugin",
+              location = "node_modules",
+              languages = { "javascript", "typescript", "vue" },
+            },
+          },
+        },
+        filetypes = {
+          "javascript",
+          "typescript",
+          "vue",
+        },
+      },
     },
     -- customize how language servers are attached
     handlers = {

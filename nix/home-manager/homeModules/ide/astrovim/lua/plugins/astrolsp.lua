03cd4307f27d64ec89d71d56528746c27a516858
Adam Engebretson <adam@enge.me>
2024-05-12 14:31:50 -0500

    Configuring Gleam LSP

--- nix/home-manager/homeModules/ide/astrovim/lua/plugins/astrolsp.lua
+++ nix/home-manager/homeModules/ide/astrovim/lua/plugins/astrolsp.lua
@@ -39,6 +39,7 @@ return {
     -- enable servers that you already have installed without mason
     servers = {
       "rnix",
+      "gleam",
     },
     -- customize language server configuration options passed to `lspconfig`
     ---@diagnostic disable: missing-fields

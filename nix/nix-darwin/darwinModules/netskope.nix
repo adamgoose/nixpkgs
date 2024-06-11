80c5226670a924b6daea23ebb641e5a7ffe4e771
Adam Engebretson <adam@enge.me>
2024-06-11 14:44:16 -0500

    Adding Netskope Env

--- nix/nix-darwin/darwinModules/netskope.nix
+++ nix/nix-darwin/darwinModules/netskope.nix
@@ -7,15 +7,18 @@
   cabundle = pkgs.cacert.override {
     extraCertificateFiles = [./netskope_ca.pem];
   };
+
+  bundlePath = "${cabundle}/etc/ssl/certs/ca-bundle.crt";
 in {
   nix = {
     extraOptions = ''
-      ssl-cert-file = ${cabundle}/etc/ssl/certs/ca-bundle.crt
+      ssl-cert-file = ${bundlePath}
     '';
   };
 
   home-manager.users.${username}.home.sessionVariables = {
-    AWS_CA_BUNDLE = "${cabundle}/etc/ssl/certs/ca-bundle.crt";
-    NODE_EXTRA_CA_CERTS = "${cabundle}/etc/ssl/certs/ca-bundle.crt";
+    AWS_CA_BUNDLE = bundlePath;
+    NETSCKOPE_CA_BUNDLE = bundlePath;
+    NODE_EXTRA_CA_CERTS = bundlePath;
   };
 }

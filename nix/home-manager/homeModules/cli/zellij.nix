bd385116058c9f3fba4020a5f7345dee6c702bb6
Adam Engebretson <adam@enge.me>
2025-02-18 23:55:27 -0600

    Removing zjstatus config

--- nix/home-manager/homeModules/cli/zellij.nix
+++ nix/home-manager/homeModules/cli/zellij.nix
@@ -1,28 +1,15 @@
 {
   pkgs,
-  # inputs,
   ...
 }:
-# let
-#   zjstatus = inputs.zjstatus.packages.${pkgs.system}.default;
-# in
 {
   programs.zellij = {
     enable = true;
     settings = {
       pane_frames = false;
-      plugins = {
-        # zjstatus = {
-        #   _props = {
-        #     location = "file:${zjstatus}/bin/zjstatus.wasm";
-        #   };
-        # };
-      };
     };
   };
 
-  # xdg.configFile."zellij/layouts/default.kdl".source = ./files/zellij-layout.kdl;
-  # xdg.configFile."zellij/layouts/default.swap.kdl".source = ./files/zellij-layout.swap.kdl;
 
   xdg.configFile."ghostty/config".text = ''
     command = ${pkgs.zellij}/bin/zellij attach --create
